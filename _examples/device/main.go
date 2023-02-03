9c38dbd6db37c74ea3e79332e06126d3e3fa7855
Rido <rido-min@users.noreply.github.com>
2023-02-03 22:00:47 +0000

    upd container, and sample

--- _examples/device/main.go
+++ _examples/device/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/amenzhinsky/iothub/iotdevice"
 	iotmqtt "github.com/amenzhinsky/iothub/iotdevice/transport/mqtt"
 )
 
 func main() {
+	fmt.Println(os.Getenv("IOTHUB_DEVICE_CONNECTION_STRING"))
 	c, err := iotdevice.NewFromConnectionString(
 		iotmqtt.New(), os.Getenv("IOTHUB_DEVICE_CONNECTION_STRING"),
 	)
@@ -21,6 +24,15 @@ func main() {
 	if err = c.Connect(context.Background()); err != nil {
 		log.Fatal(err)
 	}
+
+	s := fmt.Sprintf("%d", time.Now().UnixNano())
+	v, err := c.UpdateTwinState(context.Background(), map[string]interface{}{
+		"ts": s,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("version: %d", v)
 
 	// send a device-to-cloud message
 	if err = c.SendEvent(context.Background(), []byte("hello")); err != nil {
